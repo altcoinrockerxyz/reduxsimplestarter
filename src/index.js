7306a223f74a1b93b0a27f27b38b7b8edb96e052
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-10-09 11:51:10 +0800

    Lecture 217: Intro to JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component. This component should produce
+// some HTML. NOTE: const is an ES6 or ES2016 syntax
+const App = function() {
+  return <div>Hi!</div>; // this HTML stuff is called JSX
+};
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it
+// on the page (in the DOM))
